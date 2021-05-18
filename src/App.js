c7c0f72d4d37b096ea111d09993603d21d658a8b
jyotiprakash-m <teachmeonline365@gmail.com>
2021-05-18 11:42:52 +0530

    search and header component completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Result from './components/Result'
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
   return (
-    <div className="app">
+    <div className={darkMode ? "app darkbg1" : "app lightbg1"}>
       {/* header */}
-
-
-      {/* result */}
-      <Result />
-
+      <div className="app__container">
+        <div className="header">
+          <div className="header__logo">
+            <h1 className={darkMode ? "darkText2" : "lightText1"}>Job <span style={{ color: "#f2a365" }}>Hunter</span> </h1>
+          </div>
+          <div className="header__switch">
+            <label className="switch">
+              <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+              <span className="slider round"> </span>
+            </label>
+          </div>
+        </div>
+        {/* result */}
+        <Result darkMode={darkMode} />
+      </div>
     </div>
   );
 }
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function Result() {
+function Result({ darkMode }) {
+    const [term, setTerm] = useState("node")
+    const [posts, setPosts] = useState([])
+    useEffect(() => {
+        fetch(`/positions.json?search=${term}`)
+            .then((response) => response.json())
+            .then((json) => setPosts(json))
+    }, [term]);
+    console.log(darkMode)
+    console.log(posts)
     return (
-        <div>
-            <h1>This is a result page</h1>
+        <div className="result">
+            <div className="result__InputContainer">
+                <div className="result__Input">
+                    <input type="text" value={term} onChange={(e) => setTerm(e.target.value)} />
+                </div>
+                <div className="result__InputLogo">
+                    <img src="./searchlogo.png" />
+                </div>
+            </div>
+            <div className="posts">
+                {posts.map((post, index) => {
+                    return (
+                        index < 20 &&
+                        <div key={post.id} className="post">
+                        </div>
+
+                    )
+                }
+                )}
+
+            </div>
         </div>
     )
 }
